f0f97710dd5a44654203e71dfb0550bb2b1e7bd2
PatrykMobile <patrykdabrowski0@gmail.com>
2020-03-30 20:05:11 +0200

    initial one

---
+++ ABD_cw3/Controllers/StudentsController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ABD_cw3.Controllers
+{
+    [ApiController]
+
+    public class StudentsController
+    {
+        public StudentsController()
+        {
+        }
+    }
+}
