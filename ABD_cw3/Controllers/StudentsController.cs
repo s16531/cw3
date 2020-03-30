c854fdc20d038d3abb458817b79c0d4f2321cca3
PatrykMobile <patrykdabrowski0@gmail.com>
2020-03-30 20:33:19 +0200

    Done

--- ABD_cw3/Controllers/StudentsController.cs
+++ ABD_cw3/Controllers/StudentsController.cs
@@ -1,12 +1,61 @@
 ﻿using System;
+using ABD_cw3.DAL;
+using ABD_cw3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
 namespace ABD_cw3.Controllers
 {
     [ApiController]
+    [Route("api/students")]
 
-    public class StudentsController
+    public class StudentsController : ControllerBase
     {
-        public StudentsController()
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
         {
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 2)
+            {
+                return Ok("Kowalski");
+            }
+            else
+            {
+                return NotFound("Nie znaleziono");
+            }
+            
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent([FromBody]Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutStudent(int id)
+        {
+            return Ok("CHANGED");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok("DELTED");
         }
     }
 }
---
+++ ABD_cw3/DAL/IDbService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using ABD_cw3.Models;
+
+namespace ABD_cw3.DAL
+{
+    public interface IDbService
+    {
+        IEnumerable<Student> GetStudents();
+    }
+}
