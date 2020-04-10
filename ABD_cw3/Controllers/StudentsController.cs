2d89c23800bff9d25f51ef241b0b07352cd9cb6f
PatrykMobile <patrykdabrowski0@gmail.com>
2020-04-10 21:52:49 +0200

    [ZADANIE4] Done

--- ABD_cw3/Controllers/StudentsController.cs
+++ ABD_cw3/Controllers/StudentsController.cs
@@ -45,6 +45,61 @@ namespace ABD_cw3.Controllers
             return Ok(listOfStudents);
         }
 
+        [HttpGet]
+        public IActionResult GetEnrolments(string indexNumber)
+        {
+            var listOfStudents = new List<Student>();
+            using var con = new SqlConnection("Data Source=db-mssql;Initial Catalog=s16531;Integrated Security=True");
+            using (var com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "select * from Enrollment E where E.IdEnrollment in (select IdEnrollment from Student where IndexNumber = @indexNumber";
+                con.Open();
+                var dr = com.ExecuteReader();
+                var en = new String("");
+                while (dr.Read())
+                {
+                   
+                    en += dr["IdEnrollment"].ToString();
+                    en += dr["Semester"].ToString() + " ";
+                    en += dr["IdStudy"].ToString() + " ";
+                    en += dr["StartDate"].ToString();
+                }
+                return Ok(en);
+            }
+    
+        }
+
+        [HttpGet("{indexNumber}")]
+        public IActionResult getStudent(string indexNumber)
+        {
+            using var con = new SqlConnection("Data Source=db-mssql;Initial Catalog=s16531;Integrated Security=True");
+            using (var com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "select * from Student where IndexNumber = @indexNumber";
+                SqlParameter parameter = new SqlParameter();
+                parameter.Value = indexNumber;
+                parameter.ParameterName = "index";
+                com.Parameters.Add(parameter);
+
+                con.Open();
+                SqlDataReader dr = com.ExecuteReader();
+                if (dr.Read())
+                {
+                    var st = new Student();
+                    st.FirstName = dr["FirstName"].ToString();
+                    st.LastName = dr["LastName"].ToString();
+                    st.IndexNumber = dr["IndexNumber"].ToString();
+                    return Ok(st);
+                }
+
+                return NotFound();
+            }
+
+
+        }
+
 
         [HttpGet("{id}")]
         public IActionResult GetStudent(int id)
