2c87ef9d51e67c89c1142ad0dbcb5f95fc954a8e
PatrykMobile <patrykdabrowski0@gmail.com>
2020-05-25 22:01:09 +0200

    7.3 done

---
+++ ABD_cw3/Handlers/PasswordHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace ABD_cw3.Handlers
+{
+    public class PasswordHandler
+    {
+        public static string CreateHash(string password, string salt)
+        {
+            var valueBytes = KeyDerivation.Pbkdf2(
+                password: password,
+                salt: Encoding.UTF8.GetBytes(salt),
+                prf: KeyDerivationPrf.HMACSHA512,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8);
+            return Convert.ToBase64String(valueBytes);
+        }
+    }
+}
